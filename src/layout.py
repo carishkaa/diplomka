3c38a16850a9eb447201037aaff5cc89d0c30378
Karina Balagazova <carishkaa@gmail.com>
2024-08-27 20:35:35 +0500

    add kolecko_4_enter_50

--- src/layout.py
+++ src/layout.py
@@ -52,8 +52,18 @@ def get_layout(type):
         last_column = [(i, COORDINATE_Y-1) for i in range(1,COORDINATE_X-1)]
         last_row = [(COORDINATE_X-1, i) for i in range(1,COORDINATE_Y-1)]
         first_column = [(i, 0) for i in range(1,COORDINATE_X-1)]
-        print(first_row, last_column, last_row, first_column)
-        print(last_row)
+        all_posible_positions = first_row + last_column + last_row[::-1] + first_column[::-1]
+    elif type == 'kolecko_4_enter_50':
+        COORDINATE_X = 16
+        COORDINATE_Y = 15
+        restricted_area = np.zeros((COORDINATE_X, COORDINATE_Y))
+        restricted_area[1:COORDINATE_X-1, 1:COORDINATE_Y-1] = 1
+        restricted_area[[0, 0, -1, -1], [0, -1, -1, 0]] = 1
+        # positions as a tape: first row, last column, last row, first column
+        first_row = [(0, i) for i in range(1,COORDINATE_Y-1)]
+        last_column = [(i, COORDINATE_Y-1) for i in range(1,COORDINATE_X-1)]
+        last_row = [(COORDINATE_X-1, i) for i in range(1,COORDINATE_Y-1)]
+        first_column = [(i, 0) for i in range(1,COORDINATE_X-1)]
         all_posible_positions = first_row + last_column + last_row[::-1] + first_column[::-1]
     elif type == 'square':
         COORDINATE_X = 12
