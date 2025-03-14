431c51bb21c290d91c43f8f910b11a5b79b34f93
Karina Balagazova <carishkaa@gmail.com>
2025-03-14 14:41:55 +0100

    Data generator: Add min max drugs per sample

--- src/data_generator/main.py
+++ src/data_generator/main.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import TypedDict
 import warnings
 import numpy as np
 from statsmodels.stats.correlation_tools import cov_nearest
@@ -10,34 +11,42 @@ def is_pos_def(x):
     return np.all(np.linalg.eigvals(x) >= 0)
 
 # https://research.wu.ac.at/ws/portalfiles/portal/18952613/document.pdf
-def generate_multivariate_binary(num_vars, num_samples, marginal_probs, corr_matrix):
+def generate_multivariate_binary(num_vars, num_samples, marginal_probs, corr_matrix, threshold_drugs_per_sample: tuple=None):
     if len(marginal_probs) != num_vars:
         raise ValueError("Number of variables does not match the length of the means vector.")
-    
+
     if corr_matrix.shape != (num_vars, num_vars):
         raise ValueError("Correlation matrix dimensions do not match the number of variables.")
-    
-    commonprob = bnd.bincorr2commonprob(margprob=marginal_probs, 
-                                            bincorr=corr_matrix)
+
+    commonprob = bnd.bincorr2commonprob(margprob=marginal_probs, bincorr=corr_matrix)
     sigma = bnd.commonprob2sigma(commonprob)
     if not is_pos_def(sigma):
-        not_pos_def_sigma = sigma
         sigma = cov_nearest(sigma)
 
     µ, Σ = norm.ppf(marginal_probs), sigma
     mvn = multivariate_normal(µ, Σ, allow_singular=True)
-    sample = mvn.rvs(size=num_samples)
-    # sample_binary = sample > 0
-    # uniformly from 3 to 7 
+    sample_norm = mvn.rvs(size=num_samples)
 
-    # get 3 maximal elements of sample
-    sample_binary = np.zeros_like(sample)
-    for i in range(sample.shape[0]):
-        num_of_drugs = np.random.choice([3, 4, 5, 6, 7, 8], p=[0.92, 0.07, 0.0045, 0.0039, 0.0015, 0.0001])
-        indices = np.argpartition(sample[i], -num_of_drugs)[-num_of_drugs:]
+    sample_metadata: Metadata = { 'mu': µ, 'sigma': Σ, 'mvn': mvn, 'sample_norm': sample_norm }
+    if threshold_drugs_per_sample is None:
+        # From the paper: convert to binary by thresholding the normal samples at 0.
+        return sample_norm > 0, sample_metadata
+
+    # Convert to binary by choosing top k drugs per sample.
+    min_drugs_per_sample, max_drugs_per_sample = threshold_drugs_per_sample
+    drugs_per_sample = np.random.choice(range(min_drugs_per_sample, max_drugs_per_sample + 1), size=num_samples) # TODO maybe not uniformly?
+    sample_binary = np.zeros_like(sample_norm)
+    for i in range(num_samples):
+        indices = np.argpartition(sample_norm[i], -drugs_per_sample[i])[-drugs_per_sample[i]:]
         sample_binary[i][indices] = 1
 
-    return sample_binary, µ, Σ, mvn.rvs(size=num_samples)
+    return sample_binary, sample_metadata
+
+class Metadata(TypedDict):
+    mu: np.ndarray
+    sigma: np.ndarray
+    mvn: any
+    sample_norm: np.ndarray
 
 def filter_rows(sample, mu, sigma, target_numvars):
     """ Filter rows with number of ones in the range [3, 7] and add new rows if necessary. """
@@ -71,21 +80,14 @@ def generate_dosages(capsule, dosages):
         generated_dosages.append(np.random.randint(min_dosage, max_dosage))
     return generated_dosages
 
-def load_means(file_path):
-    return np.loadtxt(file_path, delimiter=",")
-
-def load_corr_matrix(file_path):
-    return np.loadtxt(file_path, delimiter=",")
-
-def load_dosages(file_path):
-    dosages = pd.read_csv(file_path, sep=';')
+def load_dosages():
+    dosages = pd.read_csv("drugs_dosing.csv", sep=';')
     dosages = dosages[['Drug Names', 'MIN units [pcs]', 'MAX units [pcs]']]
     dosages['Average units [pcs]'] = (dosages['MIN units [pcs]'] + dosages['MAX units [pcs]']) / 2
     return dosages
 
-def generator(num_vars, num_samples, marginals, corr_matrix, cnames, dosages):
-    sample, mu, sigma, _ = generate_multivariate_binary(num_vars, num_samples, marginals, corr_matrix)
-    sample = filter_rows(sample, mu, sigma, num_samples) # number of drugs [3, 7]
+def generator(num_vars, num_samples, marginals, corr_matrix, cnames, dosages, threshold_drugs_per_sample):
+    sample, _ = generate_multivariate_binary(num_vars, num_samples, marginals, corr_matrix, threshold_drugs_per_sample)
     capsules = binary2capsules(sample, cnames)
     capsules['SEQN'] = capsules.index
     capsules['dosages'] = capsules.apply(lambda x: generate_dosages(x['drug_names'], dosages), axis=1)
@@ -93,30 +95,30 @@ def generator(num_vars, num_samples, marginals, corr_matrix, cnames, dosages):
     res_capsules = capsules[['SEQN', 'drug_names', 'dosages']]
     return res_capsules
 
-def main(num_vars, num_samples, marginals_filename, corr_matrix_filename, cnames_filename, dosages_filename, output_filename):
-    means = load_means(marginals_filename)
-    corr_matrix = load_corr_matrix(corr_matrix_filename)
-    dosages = load_dosages(dosages_filename)
-    drug_names = pd.read_csv(cnames_filename, header=None)[0].to_list()
+def main(num_vars, num_samples, marginals_filename, corr_matrix_filename, sorted_names_filename, output_filename, threshold_drugs_per_sample):
+    means = np.loadtxt(marginals_filename, delimiter=",")
+    corr_matrix = np.loadtxt(corr_matrix_filename, delimiter=",")
+    dosages = load_dosages()
+    drug_names = pd.read_csv(sorted_names_filename, header=None)[0].to_list()
 
-    df = generator(num_vars, num_samples, means, corr_matrix, drug_names, dosages)
+    df = generator(num_vars, num_samples, means, corr_matrix, drug_names, dosages, threshold_drugs_per_sample)
     df.to_csv(output_filename, sep=';', index=False)
     return df
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process input arguments.")
+    parser = argparse.ArgumentParser(description="Patients generator", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("-v", "--num_vars", type=int, help="Number of variables", default=40)
-    parser.add_argument("-n", "--num_samples", type=int, help="Number of samples to generate", default=1000)
-    parser.add_argument("-m", "--marginals", type=argparse.FileType('r'), help="CSV file containing marginal probabilities")
-    parser.add_argument("-c", "--corr_matrix", type=argparse.FileType('r'), help="CSV file containing the correlation matrix")
-    parser.add_argument("--names_file", type=argparse.FileType('r'), help="CSV file containing the drug names in the same order as marginals and correlation matrix")
-    parser.add_argument("-d", "--dosages", type=argparse.FileType('r'), help="Dosages")
-    default_output_file = 'generated_capsules_with_dosages.csv'
+    parser.add_argument("-v", "--num-vars", type=int, help="Number of variables (drugs)", default=40)
+    parser.add_argument("-n", "--num-samples", type=int, help="Number of samples (patients) to generate", default=1000)
+    parser.add_argument("-p", "--marginal-probas", type=argparse.FileType('r'), help="CSV file containing marginal probabilities of each drug", metavar=('MARGINAL_PROBAS_FILE'), required=True)
+    parser.add_argument("-c", "--corr-matrix", type=argparse.FileType('r'), help="CSV file containing the correlation matrix of the drugs", metavar=('CORR_MATRIX_FILE'), required=True)
+    parser.add_argument("-s", "--sorted-names", type=argparse.FileType('r'), help="CSV file containing the drug names in the same order as marginals and correlation matrix", metavar=('SORTED_NAMES_FILE'))
+    parser.add_argument("-d", "--drugs-per-sample", type=int, nargs=2, metavar=('MIN', 'MAX'), help="Minimum and maximum number of drugs per sample. If not provided, it will not be limited and vary naturally.")
+    parser.add_argument("-o", "--output", type=str, help="Output file name", default='generated_capsules_with_dosages.csv', metavar=('OUTPUT_FILE'))
 
     args = parser.parse_args()
 
     with warnings.catch_warnings(record=True) as recorded_warnings:
-        main(args.num_vars, args.num_samples, args.marginals_file, args.corr_matrix_file, args.cnames_file, args.dosages_file, default_output_file)
+        main(args.num_vars, args.num_samples, args.marginal_probas, args.corr_matrix, args.sorted_names, args.output, args.drugs_per_sample)
 
-    print("Generated successfully and saved to \"" + default_output_file + "\".")
+    print("Generated successfully and saved to \"" + args.output + "\".")
--- src/data_generator/readme.md
+++ src/data_generator/readme.md
@@ -1,8 +1,29 @@
 # Data Generator
 
 ## Run
+```bash
+python3 main.py -v 40 -n 1000 -p feat_probs.csv -c correlation_matrix.csv --sorted-names drug_names.csv -d 3 7
 ```
-python3 main.py 40 1000 feat_probs.csv correlation_matrix.csv drug_names.csv drugs_dosing.csv
+
 ```
+  -h, --help            show this help message and exit
+  -v NUM_VARS, --num-vars NUM_VARS
+                        Number of variables (drugs) (default: 40)
+  -n NUM_SAMPLES, --num-samples NUM_SAMPLES
+                        Number of samples (patients) to generate (default: 1000)
+  -p MARGINAL_PROBAS_FILE, --marginal-probas MARGINAL_PROBAS_FILE
+                        CSV file containing marginal probabilities of each drug (default: None)
+  -c CORR_MATRIX_FILE, --corr-matrix CORR_MATRIX_FILE
+                        CSV file containing the correlation matrix of the drugs (default: None)
+  -s SORTED_NAMES_FILE, --sorted-names SORTED_NAMES_FILE
+                        CSV file containing the drug names in the same order as marginals and correlation matrix (default: None)
+  -d MIN MAX, --drugs-per-sample MIN MAX
+                        Minimum and maximum number of drugs per sample. If not provided, it will not be limited and vary
+                        naturally. (default: None)
+  -o OUTPUT_FILE, --output OUTPUT_FILE
+                        Output file name (default: generated_capsules_with_dosages.csv)
+```
+
+Based on https://research.wu.ac.at/ws/portalfiles/portal/18952613/document.pdf, but if min and max number of drugs per sample provided, we just pick top `k = [min, max]` elements from normal sample during converting to binary. 
 
 todo description, library requirements
